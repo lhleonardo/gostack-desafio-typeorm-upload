5674ebfa22b722d1444536ceab7b1bf83fbdee3e
Leonardo Braz <lhleonardo05@gmail.com>
2020-07-02 19:48:59 -0300

    mapeamento de entidades

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity({ name: 'transactions' })
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column({ type: 'enum', enum: ['income', 'outcome'] })
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
